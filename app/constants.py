c6003be905429bb4df6b3698502a8b0aaf818c61
spl3en <spl3en.contact@gmail.com>
2018-09-17 11:35:44 +0200

    Update to mainnet HTTP Provider

--- app/constants.py
+++ app/constants.py
@@ -1,3 +1,4 @@
 # HTTP_PROVIDER = "http://13.209.103.183:9000/api/v3"
-HTTP_PROVIDER = "https://bicon.net.solidwallet.io/api/v3"
+# HTTP_PROVIDER = "https://bicon.net.solidwallet.io/api/v3"
+HTTP_PROVIDER = "https://wallet.icon.foundation/api/v3"
 GITHUB_REPOSITORY = "https://github.com/Spl3en/icon-tracker"
